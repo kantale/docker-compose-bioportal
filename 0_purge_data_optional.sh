596abb1ced049f55576381199efa3f2e68ed7dd1
Andon Tchechmedjiev <twk.theainur@gmail.com>
2017-01-20 18:03:56 +0100

    Fixed a solr problem. Now passes all the ncbo_core integration tests.

--- 0_purge_data_optional.sh
+++ 0_purge_data_optional.sh
@@ -5,5 +5,6 @@ sudo rm -fr data/4store/*
 sudo rm -fr data/redis/goo/*
 sudo rm -fr data/redis/http/*
 sudo rm -fr data/redis/annotator/*
-
-echo -e "1000\tNULL" > data/mgrep/dictionary.txt 
+sudo rm -fr data/solr/core1/data
+sudo rm -fr data/solr/core2/data
+sudo bash -c 'echo -e "1000\tNULL" > data/mgrep/dictionary.txt'
