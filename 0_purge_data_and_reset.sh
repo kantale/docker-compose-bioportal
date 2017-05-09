261b1889bcfbbda5634b5cbce025b50d3ee9dba9
Vincent Emonet <vincent.emonet@gmail.com>
2017-05-09 11:44:01 -0700

    Properly delete term_search_core and prop_search_core data when purging

--- 0_purge_data_and_reset.sh
+++ 0_purge_data_and_reset.sh
@@ -5,7 +5,9 @@ sudo rm -fr data/4store/*
 sudo rm -fr data/redis/goo/*
 sudo rm -fr data/redis/http/*
 sudo rm -fr data/redis/annotator/*
-sudo rm -fr data/solr/core1/data
-sudo rm -fr data/solr/core2/data
+sudo rm -fr data/solr/term_search_core1/data
+sudo rm -fr data/solr/term_search_core2/data
+sudo rm -fr data/solr/prop_search_core1/data
+sudo rm -fr data/solr/prop_search_core2/data
 sudo rm -fr data/ncbo_logs/*
 sudo bash -c 'echo -e "1000\tNULL" > data/mgrep/dictionary.txt'
