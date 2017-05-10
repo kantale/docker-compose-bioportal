bc229e2542c46bf9baa2fa4a1c4c48ef9725476f
Vincent Emonet <vincent.emonet@gmail.com>
2017-05-10 14:11:08 -0700

    now copy the dictionary.txt in data/mgrep before running mgrep when running containers for tests

--- 00_run_test_containers.sh
+++ 00_run_test_containers.sh
@@ -3,4 +3,6 @@
 # First delete all data to reset properly all containers
 ./0_purge_data_and_reset.sh
 
+cp bioportal-mgrep/resources/dictionary.txt data/mgrep/dictionary.txt
+
 docker-compose -f test.yml up --force-recreate
