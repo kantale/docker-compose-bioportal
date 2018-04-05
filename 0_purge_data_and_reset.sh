ef4a75572144fe91fbf3bed9198b723262dcb696
Andon Tchechmedjiev <twk.theainur@gmail.com>
2018-04-05 05:49:55 -0700

    fixes

--- 0_purge_data_and_reset.sh
+++ 0_purge_data_and_reset.sh
@@ -10,4 +10,5 @@ sudo rm -fr data/solr/term_search_core2/data
 sudo rm -fr data/solr/prop_search_core1/data
 sudo rm -fr data/solr/prop_search_core2/data
 sudo rm -fr data/ncbo_logs/*
+
 sudo bash -c 'echo -e "1000\tNULL" > data/mgrep/dictionary.txt'
