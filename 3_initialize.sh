60c5b208b95dc7aa49ca97d0480936f7c1d6751c
Andon Tchechmedjiev <twk.theainur@gmail.com>
2018-04-12 11:37:27 -0700

    Fixed issue with 4store initialization

--- 3_initialize.sh
+++ 3_initialize.sh
@@ -6,6 +6,8 @@ echo "Startup in progress (120s timeout before populate), showing logs:"
 timeout 120s docker-compose logs -f;
 
 
+admin/purge-4store
+admin/create-user
 
 echo "Forcing annotatior dictionary generation, this will take a while..."
 admin/init-ncbo-cron
